15ca24ae04009a0b3d54a81d921b1b91c851676f
HodaEb <92301403+HodaEb@users.noreply.github.com>
2022-02-28 23:07:22 +0330

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -55,10 +55,8 @@ def get_loader(args):
         transform_train = transforms.Compose([
             transforms.RandomResizedCrop((args.img_size, args.img_size), scale=(0.05, 1.0)),
             transforms.RandomHorizontalFlip(),
-                transforms.RandomApply([
-                  transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8
-                ),
-                transforms.RandomGrayscale(0.2),
+            transforms.RandomApply([transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8),
+            # transforms.RandomGrayscale(0.2),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
             ])
