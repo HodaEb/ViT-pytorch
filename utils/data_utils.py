683770d0054d42f3f2fd5d8c144096d06c69d9f2
HodaEb <92301403+HodaEb@users.noreply.github.com>
2022-02-28 16:35:59 +0330

    Update data_utils.py

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -33,6 +33,34 @@ def get_loader(args):
                                    train=False,
                                    download=True,
                                    transform=transform_test) if args.local_rank in [-1, 0] else None
+    elif args.dataset == "stanford40":
+        # Data
+        # transform_train = transforms.Compose([
+        #     transforms.RandomResizedCrop(224),
+        #     transforms.RandomHorizontalFlip(),
+        #     transforms.RandomApply([
+        #       transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)], p=0.8
+        #     ),
+        #     transforms.RandomGrayscale(0.2),
+        #     transforms.ToTensor(),
+        #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        # ])
+
+        # transform_test = transforms.Compose([
+        #     transforms.Resize((224, 224)),
+        #     transforms.ToTensor(),
+        #     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        # ])
+
+        trainset = datasets.ImageFolder(root='/content/Standford40/StanfordActionDataset/train',
+                                        transform=transform_train)
+        # trainloader = torch.utils.data.DataLoader(
+        #     trainset, batch_size=32, shuffle=True, num_workers=2)
+
+        testset = datasets.ImageFolder(root='/content/Standford40/StanfordActionDataset/test',
+                                        transform=transform_test)
+        # testloader = torch.utils.data.DataLoader(
+        #     testset, batch_size=32, shuffle=True, num_workers=2)
 
     else:
         trainset = datasets.CIFAR100(root="./data",
